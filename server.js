0011d88ca55240f1e53f94b0e15d63ec43b5664b
Jose Juan <josejuanab@gmail.com>
2025-02-19 13:05:32 +0100

    index

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const articleRouter = require('./routes/articles')
+
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use('/articles', articleRouter);
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hola mundo');
+    const articles = [{
+        title: 'Artiulo de prueba',
+        createdAt: new Date(),
+        description: 'Descripcion de prueba'
+    },
+    {
+        title: 'Artiulo 2',
+        createdAt: new Date(),
+        description: 'Descripcion de prueba número 2'
+    }]
+    res.render('index', { articles: articles });
 });
 
 app.listen(port, () => 
